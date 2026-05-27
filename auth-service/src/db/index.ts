ef643b5870641ecd05adcde756cf0b85c7e63b3a
Qian Gong <gongqian.de@gmail.com>
2026-05-27 20:52:13 +0200

    chore(db): configure auth database connection

---
+++ auth-service/src/db/index.ts
@@ -0,0 +1,16 @@
+import mongoose from 'mongoose';
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGODB_URI!, {
+      dbName: 'pokemon',
+    });
+
+    console.log('\x1b[35mMongoDB connected via Mongoose\x1b[0m');
+  } catch (error: unknown) {
+    console.error('MongoDB connection error:', error);
+    process.exit(1);
+  }
+};
+
+export default connectDB;
