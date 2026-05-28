3bfaea6f5ffdde50806c2346c006df9ba6a0b85f
Qian Gong <gongqian.de@gmail.com>
2026-05-28 05:20:46 +0200

    feat(auth): implement JWT and password utilities

---
+++ auth-service/src/utils/token.ts
@@ -0,0 +1,62 @@
+import jwt from 'jsonwebtoken';
+
+import type { Role, AccessTokenPayload } from '#types';
+import {
+  ACCESS_JWT_SECRET,
+  ACCESS_TOKEN_TTL,
+  REFRESH_JWT_SECRET,
+  REFRESH_TOKEN_TTL,
+} from '#config';
+import { AppError } from './AppError.ts';
+
+export const createAccessToken = async (id: string, roles: Role[]) => {
+  return jwt.sign({ roles }, ACCESS_JWT_SECRET, {
+    subject: id,
+    expiresIn: ACCESS_TOKEN_TTL,
+  });
+};
+
+export const createRefreshToken = async (id: string) => {
+  return jwt.sign({}, REFRESH_JWT_SECRET, {
+    subject: id,
+    expiresIn: REFRESH_TOKEN_TTL,
+  });
+};
+
+export const verifyToken = <T>(token: string, secret: string) => {
+  return jwt.verify(token, secret) as T;
+};
+
+export const getBearerToken = (authHeader?: string) => {
+  if (!authHeader?.startsWith('Bearer ')) {
+    throw new AppError(
+      401,
+      'Missing authentication header',
+      'NO_AUTH_HEADER',
+      'WARN',
+    );
+  }
+
+  const token = authHeader.split(' ')[1];
+
+  if (!token) {
+    throw new AppError(401, 'Missing access token', 'NO_TOKEN', 'WARN');
+  }
+
+  return token;
+};
+
+export const parseAccessToken = (authHeader?: string) => {
+  const token = getBearerToken(authHeader);
+
+  const payload = verifyToken<AccessTokenPayload>(token, ACCESS_JWT_SECRET);
+
+  if (!payload.sub || !payload.roles) {
+    throw new AppError(401, 'Invalid token payload', 'INVALID_TOKEN', 'WARN');
+  }
+
+  return {
+    id: payload.sub,
+    roles: payload.roles,
+  };
+};
