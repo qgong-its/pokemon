dc04905516266596fa403b14e7fdedbca50c186e
Qian Gong <gongqian.de@gmail.com>
2026-06-01 09:06:30 +0200

    refactor(config): use CLIENT_BASE_URL for CORS configuration

--- auth-service/src/middleware/cors.middleware.ts
+++ auth-service/src/middleware/cors.middleware.ts
@@ -2,7 +2,7 @@ import cors from 'cors';
 
 const allowedOrigins =
   process.env.NODE_ENV === 'production'
-    ? (process.env.FRONTEND_URL?.split(',') ?? [])
+    ? (process.env.CLIENT_BASE_URL?.split(',') ?? [])
     : ['http://localhost:5173'];
 
 export const corsHandler = cors({
--- auth-service/src/middleware/index.ts
+++ auth-service/src/middleware/index.ts
@@ -2,4 +2,4 @@ export * from './auth.middleware.ts';
 export * from './basicErrorHandler.ts';
 export * from './extendedErrorHandler.ts';
 export * from './validate.ts';
-export * from './corsHandler.ts';
+export * from './cors.middleware.ts';
